31d96b700c44a9f8caddc9ad2d3bcbf35e08cde5
palco003 <palco003@fiu.edu>
2017-10-02 19:19:02 -0400

    New build

---
+++ Code/html/dev/build/temp/production/MobileJudge/sass/config.rb
@@ -0,0 +1,2 @@
+Compass.add_project_configuration('../../../../../sass/config.rb')
+cache_path = '/usr/share/nginx/Mobile-Judge-9.0/Code/dev/build/.sass-cache'
---
+++ Code/html/dev/build/temp/production/MobileJudge/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require /usr/share/nginx/Mobile-Judge-9.0/Code/dev/app.js
